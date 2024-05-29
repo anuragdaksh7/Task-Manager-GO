eca823bc9f262ba83c22c7cafd6ed1eca55670b4
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-05-29 23:42:10 +0530

    feat: 🎸 adds create event page (UI)

--- client/app/page.js
+++ client/app/page.js
@@ -1,3 +1,7 @@
+import {IoAddOutline} from "react-icons/io5";
+import {SignedIn} from "@clerk/nextjs";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -29,6 +33,17 @@ export default function Home() {
                   <h1 className="text-md md:text-lg font-semibold pb-1 text-gray-600">Get Ahead of the crowd with TaskIt</h1>
                   <p className="text-sm md:text-md font-bold px-3 rounded-md border-black py-1 border-[1px] w-fit">Comes with AI✨</p>
               </div>
+          <SignedIn>
+              <Link href={"/create-task"}>
+                      <div
+                          className=" absolute px-2 cursor-pointer py-1 z-10 bottom-[20px] right-[40px] border-[1px] border-black rounded-full scale-150">
+                          <div className="flex gap-2">
+                              <IoAddOutline/>
+                              <p className="text-xs  ">Add New Task</p>
+                          </div>
+                      </div>
+              </Link>
+          </SignedIn>
       </div>
   )
 }
--- client/components/clock.js
+++ client/components/clock.js
@@ -1,17 +1,20 @@
 "use client"
 
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Clock () {
-    const [time, setTime] = useState(new Date())
-    setTimeout(()=>{
-        setTime(new Date())
-    }, 1000)
+    const [time, setTime] = useState(new Date("05/14/2004"))
+    const [timeString, setTimeString] = useState((""+time.getHours()).padStart(2,"0")+":"+(""+time.getMinutes()).padStart(2,"0")+":"+(""+time.getSeconds()).padStart(2,"0"))
+    useEffect(() => {
+        setTimeout(()=>{
+            setTime(new Date())
+        }, 1000)
+        setTimeString((""+time.getHours()).padStart(2,"0")+":"+(""+time.getMinutes()).padStart(2,"0")+":"+(""+time.getSeconds()).padStart(2,"0"))
+    });
+
     return (
         <div>
-            {
-                (""+time.getHours()).padStart(2,"0")+":"+(""+time.getMinutes()).padStart(2,"0")+":"+(""+time.getSeconds()).padStart(2,"0")
-            }
+            <p>{timeString}</p>
         </div>
     )
 }
